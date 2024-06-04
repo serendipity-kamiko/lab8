7cafad99662d670e1dbc5ab514767c5c88e2e5ec
serendipity-kamiko <165747398+serendipity-kamiko@users.noreply.github.com>
2024-06-04 15:48:33 +0800

    Update Renderer.cpp

--- Renderer.cpp
+++ Renderer.cpp
@@ -229,7 +229,15 @@ void Renderer::Render(const Scene& scene)
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
-
+            x = (2 * ((i + 0.5) / scene.width -0.5) * imageAspectRatio) * scale;
+            y = -1*(2 * ((j + 0.5) / scene.height - 0.5)) * scale;
+            float len = sqrt(x * x + y * y + (-1) * (-1)); // 计算向量的长度
+            float z=-1;
+            if (len != 0){
+                x /= len;
+                y /= len;
+                z/=len;
+            }
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
